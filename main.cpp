bc94376f0e9ad434750a8dfc3e0d9cff76576a53
Marek <mw332637@students.mimuw.edu.pl>
2017-01-15 23:37:54 +0100

    hg

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 
+using namespace std;
+
+int N;
+bool DEBUG = true;
+
+void parse_entry() {
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+
+    }
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
